ab8adce3688deb41c0ba0122a4350f12593ab83b
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2017-06-23 19:49:15 +0200

    [Random Walk] A very dirty way, but working.

--- src/daart_random_walk.cpp
+++ src/daart_random_walk.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-ros::Time last_time;
+ros::Time last_time, rotated_time;
 double omega = 0.0;
 double desiredAngle = 0.0;
 bool first = true, obstacleDetected = false, shouldStop = false;
@@ -14,7 +14,7 @@ bool first = true, obstacleDetected = false, shouldStop = false;
 double linearVel = 0.2, rotationVel = 1.4;
 double fovAcceptance = M_PI/36, fovFree = M_PI/3;
 double distanceAccepted = 0.4;
-int stopTime = 1000000;
+double stopTime = 2, countObstacleApproaches = 0;
 
 void odomCallback(const nav_msgs::Odometry odom)
 {
@@ -23,7 +23,11 @@ void odomCallback(const nav_msgs::Odometry odom)
 
 
     if(obstacleDetected && fabs(omega-desiredAngle) <= fovAcceptance)
+    {
+        rotated_time = ros::Time::now();
         obstacleDetected = false;
+        countObstacleApproaches = 0;
+    }
 }
 
 template <typename T> int sgn(T val)
@@ -41,7 +45,7 @@ double closerObstacle(sensor_msgs::LaserScan scan_msg, double centerAngle, doubl
         return scan_msg.range_min;
 
     for(;i < scan_msg.ranges.size() && scan_msg.angle_min + scan_msg.angle_increment*i < centerAngle+fov/2.0; i++)
-        if (!isnan(scan_msg.ranges.at(i)))
+        if (!isnan(scan_msg.ranges.at(i)) && scan_msg.ranges.at(i) > 0.01)
             closestObstacle = min((double)scan_msg.ranges.at(i), closestObstacle);
 
     return closestObstacle;
@@ -50,9 +54,17 @@ double closerObstacle(sensor_msgs::LaserScan scan_msg, double centerAngle, doubl
 void onNewScan(const sensor_msgs::LaserScan scan_msg)
 {
     double halfFovFree = fovFree*0.5;
-    ROS_INFO("Scan: %f", closerObstacle(scan_msg, 0, fovFree));
+    ROS_INFO("Scan: %f", closerObstacle(scan_msg, 0.0, fovFree));
+    if(obstacleDetected || (ros::Time::now() - rotated_time).toSec() <= stopTime)
+        return;
     if(closerObstacle(scan_msg, 0, fovFree) < distanceAccepted)
+        countObstacleApproaches++;
+    else
+        countObstacleApproaches = 0;
+
+    if(countObstacleApproaches >= 1) //(closerObstacle(scan_msg, 0, fovFree) < distanceAccepted)
     {
+        countObstacleApproaches = 0;
 //        double maxDistance = 0, maxAngle = -1;
 //        for(double centerAngle=scan_msg.angle_min+halfFovFree; centerAngle+halfFovFree < scan_msg.angle_max; centerAngle += scan_msg.angle_increment)
 //        {
@@ -84,18 +96,21 @@ void onNewScan(const sensor_msgs::LaserScan scan_msg)
         for( int i=1; halfFovFree+scan_msg.angle_increment*i <scan_msg.angle_max; i++)
         {
             double distObstacle = closerObstacle(scan_msg, scan_msg.angle_increment*i, fovFree);
-            if(distObstacle > maxDistance)
+            if(distObstacle > distanceAccepted && distObstacle > maxDistance)
             {
                 maxDistance = distObstacle;
                 maxAngle = scan_msg.angle_increment*i;
             }
 
             distObstacle = closerObstacle(scan_msg, -scan_msg.angle_increment*i, fovFree);
-            if(distObstacle > maxDistance)
+            if(distObstacle > distanceAccepted && distObstacle > maxDistance)
             {
                 maxDistance = distObstacle;
                 maxAngle = -scan_msg.angle_increment*i;
             }
+
+            //if(maxAngle != -1)
+                //break;
         }
 
         if(maxAngle == -1)
@@ -105,7 +120,10 @@ void onNewScan(const sensor_msgs::LaserScan scan_msg)
 
         omega = 0;
         if (!obstacleDetected)
+        {
             shouldStop = true;
+            rotated_time = ros::Time::now();
+        }
         obstacleDetected = true;
 
         ROS_INFO("desiredAngle: %f", desiredAngle);
@@ -134,36 +152,41 @@ int main(int argc, char** argv)
     ros::Subscriber sub3 = n.subscribe(ns+"/laser_0", 100, onNewScan);
     ros::Publisher cmd_pub = n.advertise<geometry_msgs::Twist>(ns+"/cmd_vel", 50);
 
+    rotated_time = ros::Time::now();
+
     ros::Rate mRate(30);
     while(n.ok())
     {
         geometry_msgs::Twist cmd_vel;
 
-        if (obstacleDetected)
+        if((ros::Time::now() - rotated_time).toSec() <= stopTime)
         {
-            if (shouldStop && stopTime != 0)
-            {
-                shouldStop = false;
-                cmd_vel.linear.x = 0.;
-                cmd_vel.angular.z = 0;
-                cmd_pub.publish(cmd_vel);
-                usleep(stopTime);
-            }
+            cmd_vel.linear.x = 0.;
+            cmd_vel.angular.z = 0;
+        }
+        else if (obstacleDetected)
+        {
+//            if (shouldStop && stopTime != 0)
+//            {
+//                shouldStop = false;
+//                cmd_vel.linear.x = 0.;
+//                cmd_vel.angular.z = 0;
+//                cmd_pub.publish(cmd_vel);
+//                usleep(stopTime);
+//            }
 
             cmd_vel.linear.x = 0.;
             cmd_vel.angular.z = rotationVel*sgn(desiredAngle-omega);
-            cmd_pub.publish(cmd_vel);
-            ROS_INFO("z = %f",cmd_vel.angular.z);
         }
         else if (!obstacleDetected)
         {
-            ROS_INFO("Forward.");
             cmd_vel.linear.x = linearVel;
             cmd_vel.angular.z = 0.;
-            cmd_pub.publish(cmd_vel);
-            ROS_INFO("x = %f",cmd_vel.linear.x);
         }
 
+        cmd_pub.publish(cmd_vel);
+
+    //    ROS_INFO("vel = %f\tomega = %f\n",cmd_vel.linear.x, cmd_vel.angular.z);
         ros::spinOnce();
         mRate.sleep();
     }
