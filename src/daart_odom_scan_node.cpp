d194563babc375c07af2e9c5d0bf9e9732d651f4
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2017-07-13 14:49:10 +0200

    [Odometry - Scan] Adds the odometry based on ICP over laser scans.
    Also creates a node to merge the rotation estimated by the scan to the linear velocity of the encoders.

---
+++ src/daart_odom_scan_node.cpp
@@ -0,0 +1,101 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose2D.h>
+#include <tf/transform_broadcaster.h>
+
+
+
+class MergeOdom
+{
+public:
+
+    MergeOdom ()
+    {
+        std::string ns = ros::this_node::getNamespace();
+
+        sub1 = n.subscribe(ns+"/odom", 100, &MergeOdom::odomCallback, this);
+        sub2 = n.subscribe(ns+"/pose2D", 100, &MergeOdom::poseCallback, this);
+        odom_pub = n.advertise<nav_msgs::Odometry>(ns+"/odom_scan", 50);
+    }
+
+    void poseCallback(const geometry_msgs::Pose2D pose2d_msg)
+    {
+        pose2d = pose2d_msg;
+    }
+
+    void odomCallback(const nav_msgs::Odometry odom_msg)
+    {
+        double yaw = pose2d.theta;
+
+        double dt = (odom_msg.header.stamp - last_time).toSec();
+        double dx = odom_msg.twist.twist.linear.x*dt*cos(yaw);
+        double dy = odom_msg.twist.twist.linear.x*dt*sin(yaw);
+
+        x += dx;
+        y += dy;
+        double omega = yaw-lastYaw;
+        lastYaw = yaw;
+        omega = (omega >  M_PI) ? 2*M_PI - omega : omega;
+        omega = (omega < -M_PI) ? omega + 2*M_PI : omega;
+        omega /= dt;
+
+        //since all odometry is 6DOF we'll need a quaternion created from yaw
+        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(yaw);
+
+        //first, we'll publish the transform over tf
+        geometry_msgs::TransformStamped odom_trans;
+        odom_trans.header.stamp = odom_msg.header.stamp;
+        odom_trans.header.frame_id = "odom_scan";
+        odom_trans.child_frame_id = "base_link_scan";
+
+        odom_trans.transform.translation.x = x;
+        odom_trans.transform.translation.y = y;
+        odom_trans.transform.translation.z = 0.0;
+        odom_trans.transform.rotation = odom_quat;
+
+        //send the transform
+        odom_broadcaster.sendTransform(odom_trans);
+
+        //next, we'll publish the odometry message over ROS
+        nav_msgs::Odometry odom;
+        odom.header.stamp = odom_msg.header.stamp;
+        odom.header.frame_id = "odom_scan";
+
+        //set the position
+        odom.pose.pose.position.x = x;
+        odom.pose.pose.position.y = y;
+        odom.pose.pose.position.z = 0.0;
+        odom.pose.pose.orientation = odom_quat;
+
+        //set the velocity
+        odom.child_frame_id = "base_link_scan";
+        odom.twist.twist.linear.x = odom_msg.twist.twist.linear.x;
+        odom.twist.twist.linear.y = odom_msg.twist.twist.linear.y;
+        odom.twist.twist.angular.z = omega;
+
+        //publish the message
+        odom_pub.publish(odom);
+
+        last_time = odom_msg.header.stamp;
+    }
+
+private:
+    ros::NodeHandle n;
+    ros::Publisher odom_pub;
+    ros::Subscriber sub1, sub2;
+    tf::TransformBroadcaster odom_broadcaster;
+
+    ros::Time last_time;
+    double x=0, y=0, lastYaw=0;
+    bool first = true;
+    geometry_msgs::Pose2D pose2d;
+};
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "daart_odom_scan");
+
+  MergeOdom mergeOdom;
+
+  ros::spin();
+}
---
+++ src/scan_copy.cpp
@@ -0,0 +1,34 @@
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+
+using namespace ros;
+
+class ScanCopier{
+
+public:
+    ScanCopier()
+    {
+        sub = n.subscribe("/scan", 1, &ScanCopier::onNewScan, this);
+        scan_pub = n.advertise<sensor_msgs::LaserScan>("/scan_odom", 50);
+    }
+
+    void onNewScan(const sensor_msgs::LaserScan scan_msg)
+    {
+        sensor_msgs::LaserScan scan_msg2 = scan_msg;
+        scan_msg2.header.frame_id = "laser_odom";
+        scan_pub.publish(scan_msg2);
+    }
+
+private:
+    NodeHandle n;
+    Subscriber sub;
+    Publisher scan_pub;
+};
+
+int main(int argc, char **argv)
+{
+    // Set up ROS.
+    init(argc, argv, "scan_copy");
+    ScanCopier scanCopier;
+    spin();
+}
